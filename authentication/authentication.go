473979f4919696d374f33142b5efbe30dd8cdab1
King <qiujinwu456@gmail.com>
2017-02-20 23:35:52 +0800

    支持正则的匹配

--- authentication/authentication.go
+++ authentication/authentication.go
@@ -2,91 +2,105 @@ package authentication
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"log"
-	"strings"
 	"github.com/qiujinwu/gin-utils/sessions"
-	"github.com/qiujinwu/gin-utils/utils"
+	"log"
+	"net/http"
+	"strings"
+	"regexp"
 )
 
-type Config struct{
+type Config struct {
 	// true 黑名单，false 白名单
 	Blacklist bool
 	// 名单列表
-	Items utils.Set
+	Items map[string]string
 
 	store sessions.Store
 }
 
-/**
- 不在黑名单或者在白名单的被忽略
-*/
-func (config *Config)need_ignore(url string)bool{
-	return config.Blacklist && !config.Items.Contains(url) ||
-		!config.Blacklist && config.Items.Contains(url)
+// regexp.MatchString(pat, src)
+func (config *Config) contain(url string) bool {
+	for k, v := range config.Items {
+		if v != ""{
+			if match,err := regexp.MatchString(v, url);match && err == nil{
+				return true
+			}
+		}else{
+			if k == url{
+				return true
+			}
+		}
+	}
+	return false
 }
 
-func newConfig(blacklist bool) *Config{
+/**
+不在黑名单或者在白名单的被忽略
+*/
+func (config *Config) need_ignore(url string) bool {
+	return config.Blacklist && !config.contain(url) ||
+		!config.Blacklist && config.contain(url)
+}
+
+func newConfig(blacklist bool) *Config {
 	return &Config{
 		Blacklist: blacklist,
-		Items:utils.NewHashSet(),
+		Items:     make(map[string]string),
 	}
 }
-
 
 //--------------------------------------
 var _config *Config = nil
 var _handle gin.HandlerFunc = nil
 
-func AddUrl(url string){
-	if _config == nil{
+func AddUrl(url string,regex string) {
+	if _config == nil {
 		log.Fatal("add url before new filter")
 		return
 	}
-	_config.Items.Add(url)
+	_config.Items[url] = regex
 }
 
-func Login(c *gin.Context){
-	if _config == nil{
+func Login(c *gin.Context) {
+	if _config == nil {
 		log.Fatal("add url before new filter")
 		return
 	}
-	session_inst, _ := _config.store.Get(c,"session")
+	session_inst, _ := _config.store.Get(c, "session")
 	session_inst.Set("count", 1)
 	session_inst.Save()
 }
 
-func Logout(c* gin.Context){
-	if _config == nil{
+func Logout(c *gin.Context) {
+	if _config == nil {
 		log.Fatal("add url before new filter")
 		return
 	}
 
-	_config.store.Delete(c,"session")
+	_config.store.Delete(c, "session")
 }
 
-
-func NewFilter(blacklist bool,store sessions.Store) gin.HandlerFunc {
-	if _handle != nil{
+func NewFilter(blacklist bool, store sessions.Store) gin.HandlerFunc {
+	if _handle != nil {
 		log.Fatal("bind filter more than once")
 		return nil
 	}
-	if _config == nil{
+	if _config == nil {
 		_config = newConfig(blacklist)
 		_config.store = store
 	}
 	_handle = func(c *gin.Context) {
 		method := strings.ToUpper(c.Request.Method)
-		if _config.need_ignore(method + c.Request.RequestURI){
+		if _config.need_ignore(method + c.Request.RequestURI) {
 			c.Next()
 			return
 		}
 
-		session_inst, _ := store.Get(c,"session")
+		session_inst, _ := store.Get(c, "session")
 		v := session_inst.Get("count")
 		if v != nil {
 			c.Next()
-		}else{
+		} else {
 			c.JSON(http.StatusForbidden, gin.H{
 				"message": "StatusForbidden",
 			})
--- utils/iroute_warpper.go
+++ utils/iroute_warpper.go
@@ -9,7 +9,7 @@ type IRoutesWarapper struct {
 	base_url string
 }
 
-type RuleFunc func(string)
+type RuleFunc func(string,string)
 
 func NewIRoutesWarapper(router gin.IRoutes) *IRoutesWarapper {
 	base_url := ""
@@ -25,65 +25,76 @@ func NewIRoutesWarapper(router gin.IRoutes) *IRoutesWarapper {
 	}
 }
 
-func (irouter *IRoutesWarapper) callRuleHandles(method string,url string,rule_handles []RuleFunc){
+func (irouter *IRoutesWarapper) callRuleHandles(method string,url string,regex string,rule_handles []RuleFunc){
 	if rule_handles != nil{
 		var newurl string
 		if url[0] == '/'{
 			newurl = method + irouter.base_url + url
+			if regex != ""{
+				regex = method + irouter.base_url + regex
+			}
 		}else{
 			newurl = method + irouter.base_url + "/" + url
+			if regex != "" {
+				regex = method + irouter.base_url + "/" + regex
+			}
 		}
 		for _, value := range rule_handles {
-			value(newurl)
+			value(newurl,regex)
 		}
 	}
 }
 
 func (irouter *IRoutesWarapper) GET(url string,
 	rule_handles []RuleFunc,handlers ...gin.HandlerFunc) *IRoutesWarapper {
-	irouter.callRuleHandles("GET",url,rule_handles)
+	return irouter.GET2(url,"",rule_handles,handlers...)
+}
+
+func (irouter *IRoutesWarapper) GET2(url string,regex string,
+	rule_handles []RuleFunc,handlers ...gin.HandlerFunc) *IRoutesWarapper {
+	irouter.callRuleHandles("GET",url,regex,rule_handles,)
 	irouter.router.GET(url, handlers...)
 	return irouter
 }
 
 func (irouter *IRoutesWarapper) POST(url string,
 	rule_handles []RuleFunc,handlers ...gin.HandlerFunc) *IRoutesWarapper {
-	irouter.callRuleHandles("POST",url,rule_handles)
+	irouter.callRuleHandles("POST",url,"",rule_handles)
 	irouter.router.POST(url, handlers...)
 	return irouter
 }
 
 func (irouter *IRoutesWarapper) DELETE(url string,
 	rule_handles []RuleFunc,handlers ...gin.HandlerFunc) *IRoutesWarapper {
-	irouter.callRuleHandles("DELETE",url,rule_handles)
+	irouter.callRuleHandles("DELETE",url,"",rule_handles)
 	irouter.router.DELETE(url, handlers...)
 	return irouter
 }
 
 func (irouter *IRoutesWarapper) PATCH(url string,
 	rule_handles []RuleFunc,handlers ...gin.HandlerFunc) *IRoutesWarapper {
-	irouter.callRuleHandles("PATCH",url,rule_handles)
+	irouter.callRuleHandles("PATCH",url,"",rule_handles)
 	irouter.router.PATCH(url, handlers...)
 	return irouter
 }
 
 func (irouter *IRoutesWarapper) PUT(url string,
 	rule_handles []RuleFunc,handlers ...gin.HandlerFunc) *IRoutesWarapper {
-	irouter.callRuleHandles("PUT",url,rule_handles)
+	irouter.callRuleHandles("PUT",url,"",rule_handles)
 	irouter.router.PUT(url, handlers...)
 	return irouter
 }
 
 func (irouter *IRoutesWarapper) OPTIONS(url string,
 	rule_handles []RuleFunc,handlers ...gin.HandlerFunc) *IRoutesWarapper {
-	irouter.callRuleHandles("OPTIONS",url,rule_handles)
+	irouter.callRuleHandles("OPTIONS",url,"",rule_handles)
 	irouter.router.OPTIONS(url, handlers...)
 	return irouter
 }
 
 func (irouter *IRoutesWarapper) HEAD(url string,
 	rule_handles []RuleFunc,handlers ...gin.HandlerFunc) *IRoutesWarapper {
-	irouter.callRuleHandles("HEAD",url,rule_handles)
+	irouter.callRuleHandles("HEAD",url,"",rule_handles)
 	irouter.router.HEAD(url, handlers...)
 	return irouter
 }
