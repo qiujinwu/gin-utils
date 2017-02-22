e670d6475ca3130d60149d24cf8cd5b4053cb855
King <qiujinwu456@gmail.com>
2017-02-22 14:48:10 +0800

    支持翻译

--- binding/main.go
+++ binding/main.go
@@ -2,6 +2,8 @@ package binding
 
 import (
 	"github.com/gin-gonic/gin"
+	"gopkg.in/go-playground/validator.v8"
+	"reflect"
 )
 
 // Bind checks the Content-Type to select a binding engine automatically,
@@ -12,21 +14,50 @@ import (
 // It parses the request's body as JSON if Content-Type == "application/json" using JSON or XML as a JSON input.
 // It decodes the json payload into the struct specified as a pointer.
 // Like ParseBody() but this method also writes a 400 error if the json is not valid.
-func Bind(c * gin.Context,obj interface{}) error {
+func Bind(c * gin.Context,obj interface{}) (error,[]string) {
 	b := Default(c.Request.Method, c.ContentType())
 	return BindWith(c, obj, b)
 }
 
+
+func parseError(err error, obj interface{})(error,[]string){
+	if err == nil{
+		return nil,nil
+	}else{
+		tips := make([]string, 0)
+		real_err,ok := err.(validator.ValidationErrors)
+		if ok{
+			objt := reflect.TypeOf(obj).Elem()
+			if objt.Kind() != reflect.Struct{
+				return real_err,tips
+			}
+
+			for _, v := range real_err {
+				elem,ok := objt.FieldByName(v.Name)
+				if !ok{
+					continue
+				}
+				str,ok := elem.Tag.Lookup("error")
+				if ok{
+					// log.Printf("tag : %s\n",str)
+					tips = append(tips,str)
+				}
+			}
+			return real_err,tips
+		}else{
+			return real_err,tips
+		}
+	}
+}
+
 // BindJSON is a shortcut for c.BindWith(obj, binding.JSON)
-func BindJSON(c * gin.Context,obj interface{}) error {
+func BindJSON(c * gin.Context,obj interface{}) (error,[]string) {
 	return BindWith(c, obj, JSON)
 }
 
 // BindWith binds the passed struct pointer using the specified binding engine.
 // See the binding package.
-func BindWith(c * gin.Context,obj interface{}, b Binding) error {
-	if err := b.Bind(c.Request, obj); err != nil {
-		return err
-	}
-	return nil
+func BindWith(c * gin.Context,obj interface{}, b Binding) (error,[]string) {
+	err := b.Bind(c.Request, obj)
+	return parseError(err,obj)
 }
